36712b16844f5a2ed3e4a4ee42aed5c625ebdaa6
Lachourt <80299053+Lachourt@users.noreply.github.com>
2021-04-07 18:41:35 +1000

    Test SemiCircle class

--- test.java
+++ test.java
@@ -2,14 +2,12 @@ public class test
 {
     public static void main(String[] args)
     {
-        Point a = new Point(5.1, 4.0);
-        //Point b = new Point(4, 8.1);
-        Circle temp = new Circle();
-        temp.setCentre(a);
-        temp.setRadius(3.2);
+        Point a = new Point(4, 3);
+        Point b = new Point(5, 4);
+        SemiCircle temp = new SemiCircle();
+        temp.setPoints(a, b);
 
         PlanarShape shape = temp;
-
 
         System.out.println(shape.toString());
         System.out.println(shape.originDistance());
