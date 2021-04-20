56ecd7f16c201e7e99021a59e5efe35b616b2cb0
Lachourt <80299053+Lachourt@users.noreply.github.com>
2021-04-20 13:26:55 +1000

    Change functionality that was implemented in ShapeFactory to PA2 to better implement ShapeFactory

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -1,36 +1,43 @@
-import java.util.Locale;
-
 public class ShapeFactory
 {
-    public PlanarShape createShape(String shapeName, double[] values)
+    public static PlanarShape createShape(String shapeName, double polygonSize)
     {
         switch (shapeName.toLowerCase())
         {
             case "polygon":
             {
-                Polygon shape = new Polygon((int)values[0] + 1);
-                for (int i = 1; i < values.length - 1; i += 2)
-                {
-                    System.out.println(i);
-                    shape.addPoint(new Point(values[i], values[i + 1]), false);
-                }
-                shape.addPoint(new Point(values[1], values[2]), true);
-                return shape;
+                return new Polygon((int)polygonSize + 1);
             }
             case "circle":
             {
-                Circle shape = new Circle();
-                shape.setCentre(new Point(values[0], values[1]));
-                shape.setRadius(values[2]);
-                return shape;
+                return new Circle();
             }
             case "semicircle":
             {
-                SemiCircle shape = new SemiCircle();
-                shape.setPoints(new Point(values[0], values[1]), new Point(values[2], values[3]));
-                return shape;
+                return new SemiCircle();
             }
+            default:
+                return null;
         }
-        return null;
+    }
+    public static PlanarShape createShape(String shapeName) throws Exception
+    {
+        switch (shapeName.toLowerCase())
+        {
+            case "polygon":
+            {
+                throw new Exception("Number of points not specified for Polygon");
+            }
+            case "circle":
+            {
+                return new Circle();
+            }
+            case "semicircle":
+            {
+                return new SemiCircle();
+            }
+            default:
+                return null;
+        }
     }
 }
