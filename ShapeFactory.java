41ad9268855fffeac0e2b80e350acc939b30f7af
Lachourt <80299053+Lachourt@users.noreply.github.com>
2021-05-06 18:35:00 +1000

    Add comments to ShapeFactory

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -1,9 +1,25 @@
+/*******************************************************************************
+ ****    SENG2200 Assignment 2
+ ****    c3308061
+ ****    Lachlan Court
+ ****    06/05/2021
+ ****    This is a factory implementation method for the PlanarShape class and
+ ****    its children
+ *******************************************************************************/
+
 public class ShapeFactory
 {
+    /**
+     * Creates a shape specified by the parameter
+     * @param shapeName defining the desired shape
+     * @return a new shape
+     */
     public static PlanarShape createShape(String shapeName)
     {
+        // Switch depending on which shape was requested
         switch (shapeName.toLowerCase())
         {
+            // Return the requested shape
             case "polygon":
             {
                 return new Polygon();
@@ -16,6 +32,7 @@ public class ShapeFactory
             {
                 return new SemiCircle();
             }
+            // Return null if no valid shape was requested
             default:
                 return null;
         }
